a6f17ee14ef85d0e6b65c5a75bf3493b16ec7f6c
DmPrkp <f89522795153@gmail.com>
2020-06-19 00:05:01 +0300

    task_1

---
+++ lesson1/js/task2.js
@@ -0,0 +1,40 @@
+let webStore = new Vue({
+	el: '#app2',
+	data: {
+		sitename: 'Cafe racer moto bikes ',
+		classMoto: 'class: 125cc',
+		product1: {
+			src: 'img/m600.jpg',
+			id: '1',
+			title: 'CF 125r',
+			description: '<em>CF 125r</em> Chaparral provides online schematics & OEM parts for the US. Motorcycle Superstore provides an easy-to-use parts finder. Ships to the US, Canada, UK and Australia. Ships motorcycle parts and accessories to most countries. Sixity.com has low prices and free shipping on motorcycle parts to the US. Also check out our overview of motorcycle webshops at Bikez.info.',
+			base: 'Yamaha 125r',
+			publishing: 'Издательство "МИФ"',
+			price: 885984
+		},
+		product2: {
+			src: 'img/m1300.jpg',
+			id: '2',
+			title: 'CRX 125yr',
+			description: '<em>CRX 125yr</em> Chaparral provides online schematics & OEM parts for the US. Motorcycle Superstore provides an easy-to-use parts finder. Ships to the US, Canada, UK and Australia. Ships motorcycle parts and accessories to most countries. Sixity.com has low prices and free shipping on motorcycle parts to the US. Also check out our overview of motorcycle webshops at Bikez.info.',
+			base: 'Honda 125z',
+			publishing: 'Издательство "МИФ"',
+			price: 585984
+		},
+		product3: {
+			src: 'img/m1000.jpg',
+			id: '3',
+			title: 'XR 125cc',
+			description: '<em>XR 125cc</em> Chaparral provides online schematics & OEM parts for the US. Motorcycle Superstore provides an easy-to-use parts finder. Ships to the US, Canada, UK and Australia. Ships motorcycle parts and accessories to most countries. Sixity.com has low prices and free shipping on motorcycle parts to the US. Also check out our overview of motorcycle webshops at Bikez.info.',
+			base: 'Suzuki BRZ 125r',
+			publishing: 'Издательство "МИФ"',
+			price: 732984
+		}
+	},
+	filters: {
+		formatPrice: function (value) {
+			return value + ' ' + String.fromCharCode(8381);
+		}
+	}
+
+});
